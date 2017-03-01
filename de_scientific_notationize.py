9a96b6b0f96ebd5115b95e573068aadddca7f305
Alejandro Bustelo <abustelo@uci.edu>
2017-02-28 19:37:39 -0800

    Initial upload

---
+++ de_scientific_notationize.py
@@ -0,0 +1,38 @@
+# Alejandro Bustelo
+# 2/28/2017
+# Input: a text file with output from a MATLAB vector copied from the workspace in scientific notation
+# Output: a text file with comma-delimited numbers formatted as the arguments of a C-style array of floats with precision to 30 decimal places
+
+
+import re
+import os
+
+
+with open("input.txt", "r") as f:
+	data = f.readlines()
+
+for line in data:
+	numbers = line.split(",")
+
+intermediate = []
+output = []
+for num in numbers:
+	intermediate = re.split("e", num)
+	
+	if len(intermediate) > 1:
+		if int(intermediate[1]) < 0:
+			output.append('{0:.30f}'.format(float(intermediate[0])/(10**(abs((int(intermediate[1]))))-1)))
+		else:
+			output.append(float(intermediate[0])*10**(int(intermediate[1])))
+	else:
+		output.append('{0:.30f}'.format(float(intermediate[0])))
+	#print output[-1] #debugging
+
+with open("output.txt", "w") as f:
+	f.write("{")
+	for num in output:
+		f.write(num)
+		f.write(",")
+	f.seek(-1, os.SEEK_END) #remove last comma
+	f.truncate()
+	f.write("}")
